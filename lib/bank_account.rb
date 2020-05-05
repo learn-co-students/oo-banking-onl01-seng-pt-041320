379ee8411d55d79aa7fc9c0ded376e9231e3e8cb
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-05 20:06:10 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 class BankAccount
+
+  attr_accessor :balance, :status
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def deposit(qnty_to_deposit)
+    @balance = @balance + qnty_to_deposit
+    @balance
+  end
+  
+  def withdrawl(qnty_to_withdrawl)
+    @balance = @balance  - qnty_to_withdrawl
+    @balance
+  end
+  
+  def display_balance
+    'Your balance is $' + @balance.to_s + '.'
+  end
+  
+  def valid?
+    (@status == 'open' && @balance > 0) ? true : false
+  end
+  
+  def close_account
+    self.balance = 0
+    self.status = 'closed'
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,61 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+  @@all = []
+  
+  def initialize(sender, receiver, amount)
+    # binding.pry
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def valid?
+    valid_flag = false
+    if (sender.valid? == true && receiver.valid? == true)
+      valid_flag = true
+    else
+      valid_flag
+    end
+    valid_flag
+  end
+  
+  def execute_transaction
+    if @sender.balance > @amount
+      if @status != 'complete'
+        if self.valid? == true
+          @sender.withdrawl(@amount)
+          @receiver.deposit(@amount)
+          @status = 'complete'
+        else
+          @status = 'rejected'
+          "Transaction rejected. Please check your account balance."
+        end
+      end
+    else
+      @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+    if @status == 'complete'
+      a = self.amount
+      b = self.sender
+      c = self.receiver
+      b.deposit(a)
+      c.withdrawl(a)
+      @@all.last.status = 'reversed'
+    end
+  end
+  
 end
