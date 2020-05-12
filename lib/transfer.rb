75840eaf4726d974be7833500c66b32df91123e9
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-11 23:08:50 -0700

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :transfer, :amount
+  attr_reader :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @transfer = transfer
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    if sender.valid? == true && receiver.valid? == true
+      return true
+    else 
+      return false
+    end
+  end
+
 end
