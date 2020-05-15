a73b0f3b669a28052a7b7f583b1232c2f9426c58
Philippe Timothe <philippetimothe@gmail.com>
2020-05-15 14:40:06 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class BankAccount
+  attr_accessor 
+  attr_reader :name, :balance, :status
+
+  def initialize(name)
+    @name = name.freeze
+    @balance = 1000
+    @status = 'open'
+  end
+
+  def deposit(money)
+   @balance = balance + money
+  end
+
+
+
+
+
+
 
 end
