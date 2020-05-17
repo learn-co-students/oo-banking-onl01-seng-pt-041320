7978390db91782fc06d4bf8dcb1037c2da081c4a
eulis01 <eulis01@gmail.com>
2020-05-17 14:05:09 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class BankAccount
-
+  attr_reader :name # Getter method defined
+  attr_accessor :balance, :status
+  # initialized a bank a account with a name and 1000 value.
+  # Set status to "open" and allow to change name.
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+  # Created a method to allow #deposit of money into the bank account.
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
+  # Can display the bank account balance.
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  # Allow Account to be to close account
+  def close_account
+    self.status = "closed"
+  end
+  # Account is valid if status is open and balance is greater than 0.
+  def valid?
+  balance > 0 && status == "open" 
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :amount, :sender, :receiver
+  # initialized with a sender and receiver and can initialize a transfer.
+  def initialize(sender, amount, receiver)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
+  # Can check that both accounts are valid call on the sender and receiver.
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+  # can make transaction on both account and transfer only happens once if no enough funds reject transaction.
+  def execute_transaction
+  if valid? && sender.balance > amount && self.status == "pending"
+      until self.status == "complete"
+      sender.balance -= self.amount
+      receiver.balance += self.amount
+      self.status = "complete"
+      end
+    else
+      reject_transfer
+    end
+  end
+  # Can undo a tranfer that has already been executed.
+  def reverse_transfer
+    if self.status == "complete"
+      receiver.balance -= self.amount
+      sender.balance += aself.mount
+      self.status = "reversed"
+    end
+  end
+  # decline the transaction 
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 end
